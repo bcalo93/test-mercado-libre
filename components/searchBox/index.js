20294b434b2a3c6dcba786ba91dee9d654268c57
Brahian Calo <bcalo93@gmail.com>
2022-04-25 10:52:58 -0300

    add some searchbox work

--- components/searchBox/index.js
+++ components/searchBox/index.js
@@ -1,10 +1,14 @@
+import Image from 'next/image'
+
 import styles from './searchBox.module.scss'
 
 export default function SearchBox({ searchPagePath, placeholder }) {
   return (
     <form className={styles.form}>
-      <input />
-      <button></button>
+      <input className={styles['form-text-box']} type="text" />
+      <button className={styles['form-search-button']}>
+        <Image src="/images/ic_Search.png" layout="fill"></Image>
+      </button>
     </form>
   )
 }
