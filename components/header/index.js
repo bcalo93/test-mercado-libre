2f2950e9467cef17d99629cda9b50092a351a211
Brahian Calo <bcalo93@gmail.com>
2022-04-26 16:46:38 -0300

    add classname start adding some logic around it

--- components/header/index.js
+++ components/header/index.js
@@ -1,10 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import SearchBox from '../searchBox'
 import styles from './header.module.scss'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <SearchBox placeholder="Nunca dejes de buscar" />
+      <div className={styles['header-content-wrapper']}>
+        <Link href="/">
+          <a>
+            <Image
+              className={styles['header-logo']}
+              src="/images/Logo_ML.png"
+              width="48"
+              height="34"
+              alt="logo"
+            />
+          </a>
+        </Link>
+        <SearchBox
+          className={styles['header-search-box']}
+          placeholder="Nunca dejes de buscar"
+        />
+      </div>
     </header>
   )
 }
--- components/searchBox/index.js
+++ components/searchBox/index.js
@@ -1,9 +1,10 @@
+import classNames from 'classnames'
 import Image from 'next/image'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import styles from './searchBox.module.scss'
 
-export default function SearchBox({ placeholder }) {
+export default function SearchBox({ className, placeholder }) {
   const [searchTerm, setSearchTerm] = useState('')
   const router = useRouter()
 
@@ -18,7 +19,10 @@ export default function SearchBox({ placeholder }) {
   const handleTermChange = (event) => setSearchTerm(event.target.value)
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form
+      className={classNames(styles.form, className)}
+      onSubmit={handleSubmit}
+    >
       <input
         placeholder={placeholder}
         className={styles['form-text-box']}
@@ -27,7 +31,7 @@ export default function SearchBox({ placeholder }) {
         onChange={handleTermChange}
       />
       <button type="submit" className={styles['form-search-button']}>
-        <Image src="/images/ic_Search.png" width="16" height="16"></Image>
+        <Image src="/images/ic_Search.png" width="16" height="16" />
       </button>
     </form>
   )
