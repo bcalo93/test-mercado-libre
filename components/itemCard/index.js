7cea7dbfe0286ea2148176bfb284acc95a48bde3
Brahian Calo <bcalo93@gmail.com>
2022-04-30 17:48:42 -0300

    reorder html, work on css

--- components/itemCard/index.js
+++ components/itemCard/index.js
@@ -7,21 +7,29 @@ export default function ItemCard({ item }) {
   const { id, address, free_shipping, picture, price, title } = item
   return (
     <div className={styles.card}>
-      <img src={picture} alt={title} />
+      <img className={styles['card-picture']} src={picture} alt={title} />
       <div className={styles['card-detail']}>
-        <div className={styles['card-item_price']}>
+        <div className={styles['card-item_additional-info']}>
           <Price amount={price.amount} currency={price.currency} />
           {free_shipping ? (
-            <Image src="/images/ic_shipping.png" width="15" height="15" />
+            <Image
+              src="/images/ic_shipping.png"
+              alt="free shipping"
+              width="20"
+              height="20"
+              layout="fixed"
+            />
           ) : (
             ''
           )}
+          <span className={styles['card-item_location']}>{address.state}</span>
         </div>
-        <Link href={`/items/${id}`}>
-          <a>{title}</a>
-        </Link>
+        <div className={styles['card-item_title']}>
+          <Link href={`/items/${id}`}>
+            <a>{title}</a>
+          </Link>
+        </div>
       </div>
-      <span className={styles['card-item_location']}> {address.state}</span>
     </div>
   )
 }
